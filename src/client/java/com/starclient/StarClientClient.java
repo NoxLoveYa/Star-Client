84141059fe07598cb4b90cfacdfc42b6fa4aca54
NoxLoveYa <ewan.rondeau@gmail.com>
2025-06-06 13:31:14 +0200

    v1.0

--- src/client/java/com/starclient/StarClientClient.java
+++ src/client/java/com/starclient/StarClientClient.java
@@ -1,10 +1,6 @@
 package com.starclient;
 
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.server.command.CommandManager;
-import net.minecraft.text.Text;
 
 public class StarClientClient implements ClientModInitializer {
 
